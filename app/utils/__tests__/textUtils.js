26fab174038b951b3c50d56eb9466b3b97175cd6
Valdio Veliu <valdioveliu@gmail.com>
2019-07-28 11:36:10 +0200

    adding text utils test cases

--- app/utils/__tests__/textUtils.js
+++ app/utils/__tests__/textUtils.js
@@ -3,3 +3,8 @@ import {firstLetterToUpperCase} from '../textUtils'
 test('firstLetterToUpperCase test', () => {
   expect(firstLetterToUpperCase('hello world')).toBe('Hello world')
 })
+
+test('firstLetterToUpperCase test empty string', () => {
+  expect(firstLetterToUpperCase('')).toBe('')
+  expect(firstLetterToUpperCase()).toBe('')
+})
--- app/utils/textUtils.js
+++ app/utils/textUtils.js
@@ -1,1 +1,6 @@
+/**
+ * Convert the first character of a string to uppercase
+ * @param text
+ * @returns {string}
+ */
 export const firstLetterToUpperCase = (text = '') => text.charAt(0).toUpperCase() + text.slice(1)
